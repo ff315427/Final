e6e77d385954603bf25371ba9f4e93f4fc0c57f5
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-12-31 13:58:37 -0600

    Very large update

--- Final/Final/Form1.Designer.cs
+++ Final/Final/Form1.Designer.cs
@@ -29,12 +29,116 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.puck = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player1Score = new System.Windows.Forms.Label();
+            this.Player2Score = new System.Windows.Forms.Label();
+            this.centerLine = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.centerLine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.Color.Red;
+            this.Player1.Location = new System.Drawing.Point(12, 279);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(27, 127);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            // 
+            // puck
+            // 
+            this.puck.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.puck.Location = new System.Drawing.Point(640, 401);
+            this.puck.Name = "puck";
+            this.puck.Size = new System.Drawing.Size(27, 27);
+            this.puck.TabIndex = 1;
+            this.puck.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.ForestGreen;
+            this.Player2.Location = new System.Drawing.Point(1098, 279);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(27, 127);
+            this.Player2.TabIndex = 2;
+            this.Player2.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // Player1Score
+            // 
+            this.Player1Score.AutoSize = true;
+            this.Player1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Score.ForeColor = System.Drawing.Color.Red;
+            this.Player1Score.Location = new System.Drawing.Point(105, 9);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(55, 37);
+            this.Player1Score.TabIndex = 3;
+            this.Player1Score.Text = "00";
+            // 
+            // Player2Score
+            // 
+            this.Player2Score.AutoSize = true;
+            this.Player2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Score.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Player2Score.Location = new System.Drawing.Point(958, 9);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(55, 37);
+            this.Player2Score.TabIndex = 4;
+            this.Player2Score.Text = "00";
+            // 
+            // centerLine
+            // 
+            this.centerLine.BackColor = System.Drawing.SystemColors.Desktop;
+            this.centerLine.Location = new System.Drawing.Point(563, 0);
+            this.centerLine.Name = "centerLine";
+            this.centerLine.Size = new System.Drawing.Size(16, 647);
+            this.centerLine.TabIndex = 5;
+            this.centerLine.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1137, 646);
+            this.Controls.Add(this.Player2Score);
+            this.Controls.Add(this.Player1Score);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.puck);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.centerLine);
+            this.Name = "Form1";
+            this.Text = "Air Hockey";
+           
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.centerLine)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.PictureBox puck;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label Player1Score;
+        private System.Windows.Forms.Label Player2Score;
+        private System.Windows.Forms.PictureBox centerLine;
     }
 }
 
--- Final/Final/Form1.cs
+++ Final/Final/Form1.cs
@@ -12,9 +12,152 @@ namespace Final
 {
     public partial class Form1 : Form
     {
+        //bool goUp; //boolean to be used to detect player up position
+        //bool goDown; //boolean to be used to detect player down position
+        int speed = 5; //integer called speed holding value of 5
+        int puckX = 5; //horizontal X speed value for the ball object
+        int puckY = 5; //vertical Y speed value for the ball object
+        int score = 0; //score for the player
+        int cpuPoint = 0;// score for the CPU
+
         public Form1()
         {
             InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            KeyDown += new KeyEventHandler(Player2_KeyDown);
+            KeyPreview = true;
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)                    //Start of controls for Player 1
+        {
+            int x = Player1.Location.X;
+            int y = Player1.Location.Y;
+
+            if (e.KeyCode == Keys.Right) x += 10;
+            else if (e.KeyCode == Keys.Left) x -= 10;
+            else if (e.KeyCode == Keys.Up) y -= 10;
+            else if (e.KeyCode == Keys.Down) y += 10;
+
+            Player1.Location = new Point(x, y);
+        }                                                                   //End of controls for Player 1
+
+        private void Player2_KeyDown(object sender, KeyEventArgs e)         //Start of controls for Player 2
+        {
+            int x = Player2.Location.X;
+            int y = Player2.Location.Y;
+
+            if (e.KeyCode == Keys.D) x += 10;
+            else if (e.KeyCode == Keys.A) x -= 10;
+            else if (e.KeyCode == Keys.W) y -= 10;
+            else if (e.KeyCode == Keys.S) y += 10;
+
+            Player2.Location = new Point(x, y);
+        }                                                                   //End of controls for Player 2
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            //this is the main timer event, this event will trigger every 20 milliseconds
+
+            Player1Score.Text = "" + score; //show Player 1 score on left label
+            Player2Score.Text = "" + cpuPoint; //show Player 2 score on right label
+
+            puck.Top -= puckY; //assign the puck TOP to puck Y integer
+            puck.Left -= puckX; //assign the puck TOP to puck Y integer
+
+            //Player2.Top += speed; //assignment of the CPU top speed integer                               //CPU code
+
+
+
+
+
+            //if the score is less than 5
+
+
+
+
+
+            /*if (score < 5)                                                                                //start of CPU code
+            {
+                //if Player 2 has reached the top or gone to the bottom of the screen
+                if (Player2.Top < 0 || Player2.Top > 527)
+                {
+                    //change the speed direction so the paddle moves back up or down
+                    speed = -speed;
+                }
+            }
+            else //if score is greater than 5, increase difficulty by enabling tracking on right paddle
+            {
+                Player2.Top = puck.Top + 30;
+            }*/                                                                                             //end of CPU code
+
+            //check the score
+            //if the puck has gone past Player 1's paddle through the left
+            if (puck.Left < 0)
+            {
+                puck.Left = 560; //reset the puck to the middle of the screen
+                puckX = -puckX; //change the puck's direction
+                puckX -= 2; //increase the speed
+                cpuPoint++; //add 1 to player 2's score
+            }
+
+            //if the puck has gone past player 2's paddle on the right
+            if (puck.Left + puck.Width > ClientSize.Width)
+            {
+                puck.Left = 560; //reset the puck to the middle of the screen
+                puckX = -puckX; //change the puck's direction
+                puckX -= 2; //increase the speed
+                score++; //add 1 to player 1's score
+            }
+
+
+
+
+
+            //controlling the puck
+
+
+
+
+
+            //if the puck either reaches the top of the screen or the bottom
+            if (puck.Top < 0 || puck.Top + puck.Height > ClientSize.Height)
+            {
+                puckY = -puckY; //reverse the speed of the puck so it stays within the screen
+            }
+
+
+            //if the puck hits either paddle
+            if (puck.Bounds.IntersectsWith(Player1.Bounds) || puck.Bounds.IntersectsWith(Player2.Bounds))
+            {
+                puckX = -puckX; //bounce the puck in the other direction
+            }
+
+
+            
+
+
+            //final score and ending the game
+
+
+
+
+
+            //if Player 1's score is more than 9
+            //stop the timer and show a winning message box for Player 1
+            if (score > 9)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Congratulations, Player 1, you won this game");
+            }
+            //if Player 2's score is more than 9
+            //stop the timer and show a winning message box for Player 2
+            if (cpuPoint > 9)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Congratulations, Player 2, you won this game");
+            }
+
+            
         }
     }
 }
